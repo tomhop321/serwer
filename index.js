5cf19187a2d39db61f8302db865878426f280943
tomhop321 <85928808+tomhop321@users.noreply.github.com>
2023-08-31 13:54:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const http = require('http');
 const port = 3000;
-
+    var o = 0;
+    var i = 0;
+    var p ="w"
+    var ur = "ur"
 const requestListener = function (req, res) {
   console.log(req.url);
 
+
   // Buffer, stream, kod binarny
-debugger
+//debugger
   const chunks = [];
   req.on('data', (chunk) => {
     chunks.push(chunk);
+
+
   }).on('end', () => {
     const body = Buffer.concat(chunks).toString();
     console.log(body);
-  });
+    console.log(req.url);
 
-  res.write('<h1>Homffffdedd</h1>');
+  });
+ ur = req.url
+  if(ur[1]=="u"){
+   p = req.url;
+  console.log("pierwsz");
+i++;
+}
+o++;
+  res.write(p +" " + i + "  " + o);
+
+  //res.write(f);
   return res.end()
 };
 
